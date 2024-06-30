f964f4f281b8304841c2f875ab20011124b478de
Matus B <bialkomatus@gmail.com>
2024-06-30 13:26:47 +0200

    installed RainLab.User plugin, created App.Arrivals plugin

---
+++ plugins/app/arrival/Plugin.php
@@ -0,0 +1,18 @@
+<?php namespace App\Arrival;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Arrivals A1',
+            'description' => 'OCMS - lvl 1',
+            'author'      => 'Me :D',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+}
+?>
