3f2659c8d73a39d434a3cbe80b7852d9084aefc5
Matus B <bialkomatus@gmail.com>
2024-08-05 18:45:45 +0200

    error fix

--- plugins/app/arrival/models/Arrival.php
+++ plugins/app/arrival/models/Arrival.php
@@ -5,17 +5,24 @@ use Model;
 class Arrival extends Model
 {
     use \October\Rain\Database\Traits\Validation;
+
     public $rules = [
         'name' => 'required|min:1'
     ];
 
     public $table = 'app_arrival_arrivals';
+
     protected $fillable = [
         'name',
         'arrived_at'
     ];
+
     public $belongsTo = [
         'user' => ['RainLab\User\Models\User']
+    ];
+
+    protected $casts = [
+        'arrived_at' => 'datetime'
     ];
 
     public function getIsLateAttribute()
@@ -26,6 +33,7 @@ class Arrival extends Model
     public function beforeCreate()
     {
         $this->arrived_at = now();
+        $this->getIsLateAttribute();
     }
 }
 ?>
