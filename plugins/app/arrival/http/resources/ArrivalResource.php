73ff598f80cfc30346e205ae4696bcce895f1c92
Matus B <bialkomatus@gmail.com>
2024-08-18 15:55:23 +0200

    return user

--- plugins/app/arrival/http/resources/ArrivalResource.php
+++ plugins/app/arrival/http/resources/ArrivalResource.php
@@ -1,12 +1,15 @@
 <?php namespace App\Arrival\Http\Resources;
 
     use Illuminate\Http\Resources\Json\JsonResource;
+    use LibUser\Userapi\Http\Resources\UserResource;
 
     class ArrivalResource extends JsonResource {
 
         public function toArray($request) {
+            $userObj = new UserResource($this->user);
             return [
                 "id" => $this->id,
+                "user" => $userObj,
                 "user_id" => $this->user_id,
                 "name" => $this->name,
                 "arrived_at" => $this->arrived_at
